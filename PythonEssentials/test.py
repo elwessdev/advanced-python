24a8e1f6c1e926efe7b4c0624b6efcbaf6d622f0
elwessdev <benaliosama3@gmail.com>
2025-04-06 23:42:36 +0100

    new

---
+++ PythonEssentials/test.py
@@ -0,0 +1,9 @@
+# import platform
+# print(dir(platform))
+# print(platform.processor())
+
+# import math
+# print(math.factorial(5))
+
+# import platform
+# print(len(platform.python_version_tuple()))
