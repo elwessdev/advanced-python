3518cee8fa83837a3a881ef61a69be41f537c2b7
elwessdev <benaliosama3@gmail.com>
2025-04-24 23:49:37 +0100

    lab1 - weather app̂

--- teknolabs/lab1/weather_app/back.py
+++ teknolabs/lab1/weather_app/back.py
@@ -1,7 +1,38 @@
+from pydantic import BaseModel
+import requests as req
 from fastapi import FastAPI
 
+# Models
+class WeatherRequest(BaseModel):
+    city: str
+
+class WeatherResponse(BaseModel):
+    city: str
+    temperature: float
+    description: str
+
+
+#  FastAPI
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return "Server works!!"
+
+@app.post("/weather", response_model=WeatherResponse)
+def get_weather(data: WeatherRequest):
+    try:
+        res = req.get("http://api.openweathermap.org/data/2.5/weather",{
+            "q": data.city,
+            "appid": "f44af96718edd14bb8a7006ab45b2ae3",
+            "units": "metric"
+        })
+        res_json = res.json()
+        return {
+            "city": data.city,
+            "temperature": res_json["main"]["temp"],
+            "description": res_json["weather"][0]["description"]
+        }
+    except Exception as e:
+        return {"error": str(e)}
--- teknolabs/lab1/weather_app/front.py
+++ teknolabs/lab1/weather_app/front.py
@@ -1,3 +1,20 @@
 import streamlit as st
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+import requests as req
+
+st.title("wessWeather 🌤️")
+
+city = st.text_input("Enter city")
+
+if st.button("Get Weather"):
+    if city:
+        res = req.post(
+                "http://localhost:8000/weather",
+                json={"city": city}
+            )
+        if res.status_code == 200:
+            data = res.json()
+            st.markdown(f"<h1>Weather in {data['city']}</h1>", unsafe_allow_html=True)
+            st.write(f"🌡️ Temperature: {data['temperature']}°C")
+            st.write(f"☁️ Description: {data['description']}")
+        else:
+            st.error("City not found or error occurred.")
